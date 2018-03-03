7c8eefa8e2c29477b1fc84db554820848693bdb0
DaunGe <daunjrk@gmail.com>
2018-03-04 02:13:19 +0700

    clean branch section

---
+++ 02-gontor-branchSection/resources/js/script.js
@@ -0,0 +1,18 @@
+
+
+/* =========================================
+   				Gontor Branch
+  =======================================*/
+
+  $(document).ready(function() {
+
+    var filterizd = $('.filtr-container').filterizr({
+         //options object
+      delay: 75,
+      delayMode: 'progressive'
+
+      });
+
+   
+});
+
